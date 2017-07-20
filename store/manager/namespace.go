b2077a3eac48da1d74ffdb1343aeb6ccb6e4647c
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-07-20 17:22:10 +0200

    return number of object part of a namespace in namespace stat

--- store/manager/namespace.go
+++ store/manager/namespace.go
@@ -30,3 +30,13 @@ func (mgr *NamespaceManager) Get(label string) (*db.Namespace, error) {
 	err = ns.Decode(b)
 	return ns, err
 }
+
+// Count return the number of object present in a namespace
+func (mgr *NamespaceManager) Count(label string) (int, error) {
+	keys, err := mgr.db.List([]byte(label))
+	if err != nil {
+		return 0, err
+	}
+
+	return len(keys), nil
+}
--- store/rest/namespacesnsid_api_GetNameSpace.go
+++ store/rest/namespacesnsid_api_GetNameSpace.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/zero-os/0-stor/store/manager"
 	"github.com/zero-os/0-stor/store/stats"
 )
 
@@ -15,8 +16,8 @@ func (api NamespacesAPI) GetNameSpace(w http.ResponseWriter, r *http.Request) {
 
 	// FIXME: uncomment when the IYO middleware will create the namespace object in
 	// db automaticly
-	// mgr := manager.NewNamespaceManager(api.db)
-	//
+	mgr := manager.NewNamespaceManager(api.db)
+
 	// ns, err := mgr.Get(label)
 	// if err != nil {
 	// 	if err == db.ErrNotFound {
@@ -26,12 +27,16 @@ func (api NamespacesAPI) GetNameSpace(w http.ResponseWriter, r *http.Request) {
 	// 	}
 	// 	return
 	// }
-
+	count, err := mgr.Count(label)
+	if err != nil {
+		jsonError(w, err, http.StatusInternalServerError)
+		return
+	}
 	read, write := stats.Rate(label)
 	respBody := Namespace{
 		Label: label,
 		Stats: NamespaceStat{
-			// NrObjects: TODO
+			NrObjects:           int64(count),
 			ReadRequestPerHour:  read,
 			WriteRequestPerHour: write,
 			// SpaceAvailable: TODO
