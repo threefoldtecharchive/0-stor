faadb7baa3693daabf6f30f7f481bfe1a47918b7
Iwan Budi Kusnanto <iwanbk@gmail.com>
2017-07-26 17:07:33 +0700

    distribution.StorRestorer : distribution.Restorer that restore data from 0-stor.

--- distribution/stor_distribution.go
+++ distribution/stor_distribution.go
@@ -22,13 +22,9 @@ func NewStorDistributor(conf Config, shards []string, org, namespace string) (*S
 	}
 
 	// stor clients
-	var scs []stor.Client
-	for _, shard := range shards {
-		storClient, err := stor.NewClient(shard, org, namespace, "")
-		if err != nil {
-			return nil, err
-		}
-		scs = append(scs, storClient)
+	storClients, err := createStorClients(shards, org, namespace, "")
+	if err != nil {
+		return nil, err
 	}
 
 	// encoder
@@ -44,7 +40,7 @@ func NewStorDistributor(conf Config, shards []string, org, namespace string) (*S
 	}
 
 	return &StorDistributor{
-		storClients: scs,
+		storClients: storClients,
 		enc:         enc,
 		hasher:      hasher,
 	}, nil
@@ -65,4 +61,41 @@ func (sd StorDistributor) Write(data []byte) (written int, err error) {
 		written += len(piece)
 	}
 	return
+}
+
+// StorRestorer defines distributor that get the data
+// from 0-stor
+type StorRestorer struct {
+	dec         *Decoder
+	storClients []stor.Client
+}
+
+func NewStorRestorer(conf Config, shards []string, org, namespace string) (*StorRestorer, error) {
+	// stor clients
+	storClients, err := createStorClients(shards, org, namespace, "")
+	if err != nil {
+		return nil, err
+	}
+
+	dec, err := NewDecoder(conf.K, conf.M)
+	if err != nil {
+		return nil, err
+	}
+
+	return &StorRestorer{
+		dec:         dec,
+		storClients: storClients,
+	}, nil
+}
+
+func createStorClients(shards []string, org, namespace, iyoJWTClient string) ([]stor.Client, error) {
+	var scs []stor.Client
+	for _, shard := range shards {
+		storClient, err := stor.NewClient(shard, org, namespace, iyoJWTClient)
+		if err != nil {
+			return nil, err
+		}
+		scs = append(scs, storClient)
+	}
+	return scs, nil
 }
