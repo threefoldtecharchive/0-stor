25f87d6c24aae149770a112816fa9920660a3541
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-08-08 14:48:32 +0200

    add missing code in server main to update store start at startup

--- server/main.go
+++ server/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
+	units "github.com/docker/go-units"
 	"github.com/zero-os/0-stor/server/config"
+	"github.com/zero-os/0-stor/server/db/badger"
+	"github.com/zero-os/0-stor/server/disk"
 	"github.com/zero-os/0-stor/server/goraml"
+	"github.com/zero-os/0-stor/server/manager"
 	"github.com/zero-os/0-stor/server/storserver"
 
 	"os"
@@ -86,9 +92,10 @@ func main() {
 	app.Action = func(c *cli.Context) {
 		log.Infoln(app.Name, "version", app.Version)
 
-		// if err := ensureStoreStat(settings.DB.Dirs.Data, db); err != nil {
-		// 	log.Fatalf("Error checking server stats : %v", err)
-		// }
+		if err := ensureStoreStat(settings.DB.Dirs.Data, settings.DB.Dirs.Meta); err != nil {
+			log.Fatalln("Error computing store statistics: %v", err)
+		}
+
 		var (
 			server storserver.StoreServer
 			err    error
@@ -129,52 +136,47 @@ func main() {
 	app.Run(os.Args)
 }
 
-//
-// func ensureStoreStat(path string, db db.DB) error {
-// 	available, err := disk.FreeSpace(path)
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	namespaces, err := db.List(models.NAMESPACE_PREFIX)
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	var totalReserved uint64
-// 	for _, namespace := range namespaces {
-// 		nsStat := models.NamespaceStats{Namespace: namespace}
-// 		b, err := db.Get(nsStat.Key())
-// 		if err != nil {
-// 			return err
-// 		}
-// 		if err = nsStat.Decode(b); err != nil {
-// 			return err
-// 		}
-// 		totalReserved += nsStat.TotalSizeReserved
-// 	}
-//
-// 	available = available - totalReserved
-//
-// 	if available <= 0 {
-// 		return fmt.Errorf("total reserved size exceed availale disk space")
-// 	}
-//
-// 	stat := models.StoreStat{
-// 		SizeUsed:      totalReserved,
-// 		SizeAvailable: available,
-// 	}
-// 	b, err := stat.Encode()
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	if err = db.Set(stat.Key(), b); err != nil {
-// 		return err
-// 	}
-//
-// 	log.Infof("Space reserved : %s", units.BytesSize(float64(totalReserved)))
-// 	log.Infof("Space available : %s", units.BytesSize(float64(available)))
-//
-// 	return nil
-// }
+func ensureStoreStat(dataPath, metaPath string) error {
+	kv, err := badger.New(dataPath, metaPath)
+	if err != nil {
+		return err
+	}
+	defer kv.Close()
+
+	nsMgr := manager.NewNamespaceManager(kv)
+	statMgr := manager.NewStoreStatMgr(kv)
+
+	available, err := disk.FreeSpace(dataPath)
+	if err != nil {
+		return err
+	}
+
+	namespaces, err := kv.List([]byte(manager.NAMESPACE_PREFIX))
+	if err != nil {
+		return err
+	}
+
+	var totalReserved uint64
+	for _, namespace := range namespaces {
+		ns, err := nsMgr.Get(string(namespace))
+		if err != nil {
+			return err
+		}
+		totalReserved += ns.Reserved
+	}
+
+	available = available - totalReserved
+
+	if available <= 0 {
+		return fmt.Errorf("total reserved size exceed availale disk space")
+	}
+
+	if err := statMgr.Set(available, totalReserved); err != nil {
+		return err
+	}
+
+	log.Infof("Space reserved : %s", units.BytesSize(float64(totalReserved)))
+	log.Infof("Space available : %s", units.BytesSize(float64(available)))
+
+	return nil
+}
