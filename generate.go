c178e21392c5a1407a8c76cb9254c4df7c5c7214
Christophe de Carvalho <zaibon@users.noreply.github.com>
2017-08-08 18:08:46 +0200

    don't expose reservation API for now (#134)
    fixes #107

--- generate.go
+++ generate.go
@@ -1,4 +1,3 @@
-//go:generate  go-bindata -debug -o routes/bindata.go -pkg routes -prefix ../specs/raml/ ../specs/raml/sdstor.html
 //go:generate protoc -I specs/protobuf  specs/protobuf/store.proto --go_out=plugins=grpc:grpc_store
 
 package main
