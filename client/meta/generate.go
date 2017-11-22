f4ba7a5f54e3c5e57c04012094f1dd17cbe26503
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2017-11-22 16:31:35 -0500

    delete non-used proto code
    closes #334

--- client/meta/generate.go
+++ client/meta/generate.go
@@ -1,4 +1,3 @@
 package meta
 
 //go:generate capnp compile -I$GOPATH/src/zombiezen.com/go/capnproto2/std -ogo schema/metadata.capnp
-//go:generate protoc --proto_path=schema --go_out=schema schema/metadata.proto
--- client/meta/meta_test.go
+++ client/meta/meta_test.go
@@ -3,35 +3,10 @@ package meta
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/ugorji/go/codec"
-	"github.com/zero-os/0-stor/client/meta/schema"
-
-	"github.com/golang/protobuf/proto"
 )
-
-// func TestMarshalJSONMeta(t *testing.T) {
-// 	meta, err := New([]byte("testkey"), 1024, []string{
-// 		"http://127.0.0.1:12345",
-// 		"http://127.0.0.1:12346",
-// 		"http://127.0.0.1:12347",
-// 	})
-// 	meta.SetNumOfChunks(3)
-// 	meta.SetPrevious([]byte("previous"))
-// 	meta.SetNext([]byte("next"))
-// 	meta.SetEncrKey([]byte("secret"))
-// 	now := uint64(time.Now().UnixNano())
-// 	meta.SetEpoch(now)
-
-// 	b, err := json.Marshal(meta)
-// 	require.NoError(t, err, "fail to json encode the metadata")
-// 	expected := fmt.Sprintf(`{"configPtr":null,"encrKey":"c2VjcmV0","epoch":%d,"key":"dGVzdGtleQ==","next":"bmV4dA==","numOfChunks":3,"previous":"cHJldmlvdXM=","shard":["http://127.0.0.1:12345","http://127.0.0.1:12346","http://127.0.0.1:12347"],"size":1024}`, now)
-// 	assert.EqualValues(t, strings.TrimSpace(expected), strings.TrimSpace(string(b)))
-// }
 
 func TestMetaSize(t *testing.T) {
 	t.Run("capnp", func(t *testing.T) {
@@ -50,30 +25,6 @@ func TestMetaSize(t *testing.T) {
 		err := meta.EncodePacked(capnpBuf)
 		require.NoError(t, err)
 		fmt.Printf("size capnp-packed: %d\n", len(capnpBuf.Bytes()))
-	})
-
-	t.Run("protobuf", func(t *testing.T) {
-		meta := createProtobufMeta(t)
-
-		b, err := proto.Marshal(meta)
-		require.NoError(t, err)
-		fmt.Printf("size protobuf: %d\n", len(b))
-	})
-
-	t.Run("msgpack", func(t *testing.T) {
-		meta := createProtobufMeta(t)
-
-		var (
-			w  io.Writer
-			b  []byte
-			mh codec.MsgpackHandle
-		)
-
-		enc := codec.NewEncoder(w, &mh)
-		enc = codec.NewEncoderBytes(&b, &mh)
-		err := enc.Encode(meta)
-		require.NoError(t, err)
-		fmt.Printf("size msgpack: %d\n", len(b))
 	})
 }
 
@@ -95,23 +46,6 @@ func createCapnpMeta(t testing.TB) *Meta {
 	meta.Next = []byte("next")
 	meta.EncrKey = []byte("secret")
 	meta.Chunks = chunks
-
-	return meta
-}
-
-func createProtobufMeta(t testing.TB) *schema.ProtoMetadata {
-	meta := &schema.ProtoMetadata{}
-	meta.Key = []byte("testkey")
-	meta.Size = 1024
-	meta.Shards = make([]string, 256)
-	for i := range meta.Shards {
-		meta.Shards[i] = fmt.Sprintf("http://127.0.0.1:12345/stor-%d", i)
-	}
-	meta.NumChunks = 3
-	meta.Previous = []byte("previous")
-	meta.Next = []byte("next")
-	meta.Epoch = uint64(time.Now().Nanosecond())
-	meta.EncrKey = []byte("secret")
 
 	return meta
 }
@@ -142,57 +76,6 @@ func BenchmarkEncoding(b *testing.B) {
 				b.Error(err)
 			}
 			buf.Reset()
-		}
-	})
-
-	b.Run("protobuf", func(b *testing.B) {
-		meta := createProtobufMeta(b)
-		b.ResetTimer()
-
-		for i := 0; i < b.N; i++ {
-			encoded, err := proto.Marshal(meta)
-			if err != nil {
-				b.Error(err)
-			}
-			_ = encoded
-		}
-	})
-
-	b.Run("protobuf-buffer", func(b *testing.B) {
-		var (
-			meta = createProtobufMeta(b)
-			buf  = proto.Buffer{}
-			err  error
-		)
-		b.ResetTimer()
-
-		for i := 0; i < b.N; i++ {
-			err = buf.EncodeMessage(meta)
-			if err != nil {
-				b.Error(err)
-			}
-			buf.Reset()
-		}
-	})
-
-	b.Run("msgpack", func(b *testing.B) {
-		var (
-			meta = createProtobufMeta(b)
-			w    io.Writer
-			blob []byte
-			mh   codec.MsgpackHandle
-			err  error
-		)
-
-		enc := codec.NewEncoder(w, &mh)
-		enc = codec.NewEncoderBytes(&blob, &mh)
-		b.ResetTimer()
-
-		for i := 0; i < b.N; i++ {
-			err = enc.Encode(meta)
-			if err != nil {
-				b.Error(err)
-			}
 		}
 	})
 }
