973b0fa2ad46d2d09c6327c26c4302eb6cf28423
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2017-11-22 18:02:26 -0500

    simplify install_protoc_unix script

--- utils/scripts/install_protoc_unix.sh
+++ utils/scripts/install_protoc_unix.sh
@@ -17,22 +17,9 @@ unzip "$PROTOC_ZIP" -d "$PROTOC_DIR" ||\
 rm -rf "$PROTOC_ZIP" ||\
     (echo "couldn't remove $PROTOC_ZIP" && exit 1)
 
-# clean up old includes or make sure include namespace exists
-if [ -d "/usr/local/include/google/protobuf" ]
-then
-	sudo rm -rf "/usr/local/include/google/protobuf" ||\
-        (echo "couldn't delete /usr/local/include/google/protobuf" && exit 1)
-else
-    # make sure that at least the namespace exists
-	sudo mkdir -p "/user/local/include/google" ||\
-        (echo "couldn't create /user/local/include/google" && exit 1)
-fi
-
 # install binaries
 sudo mv "$PROTOC_DIR/bin/protoc" "/usr/local/bin/protoc" ||\
     (echo "couldn't install $PROTOC_DIR/bin/protoc" && exit 1)
-sudo mv "$PROTOC_DIR/include/google/protobuf" "/usr/local/include/google/protobuf" ||\
-    (echo "couldn't copy $PROTOC_DIR/include/google/protobuf proto std files" && exit 1)
 
 # remove unzipped and non-moved content
 rm -rf "$PROTOC_DIR" || (echo "couldn't cleanup $PROTOC_DIR" && exit 1)
